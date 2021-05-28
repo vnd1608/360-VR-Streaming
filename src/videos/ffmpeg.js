78131de02ef9bccb7c5cfb5dbbfe66f11ba40cd6
vnd1608 <ngoducvietchl1608@gmail.com>
2021-05-28 21:52:46 +0700

    ADD video

---
+++ src/videos/ffmpeg.js
@@ -0,0 +1,20 @@
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+console.log('Start enconding ...');
+ffmpeg('video1/FullHD/video1_1080.mp4', { timeout: 432000 }).addOptions([
+    '-profile:v baseline',
+    '-level 3.0',
+    '-g 25',
+    '-sc_threshold 0',
+    '-start_number 0',
+    '-hls_init_time 2',
+    '-hls_time 2',        
+    '-hls_list_size 50',   
+    '-f hls'
+]).output('video1/FullHD/output.m3u8').on('end', () => {
+    console.log('End');
+}).run();
+
